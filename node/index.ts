58d697e37777e58bc199a707feed4bcf447f469e
Vinícius Gabriel <94920663+Vini7Dev@users.noreply.github.com>
2023-06-05 15:36:54 -0300

    feat: :art: implement clients

--- node/index.ts
+++ node/index.ts
@@ -1,7 +1,17 @@
 import { PaymentProviderService } from '@vtex/payment-provider'
 
 import GetnetConnector from './connector'
+import { Clients } from './clients'
 
 export default new PaymentProviderService({
+  clients: {
+    implementation: Clients,
+    options: {
+      default: {
+        retries: 2,
+        timeout: 15000,
+      },
+    },
+  },
   connector: GetnetConnector,
 })
