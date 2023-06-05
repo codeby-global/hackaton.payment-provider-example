24a7c13532feebebf94073d5ff17365f2f78bed8
Vinícius Gabriel <94920663+Vini7Dev@users.noreply.github.com>
2023-06-05 15:30:13 -0300

    feat: :art: add vbase clients

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,7 +1,9 @@
 import type { ClientsConfig } from '@vtex/api'
 import { IOClients, LRUCache } from '@vtex/api'
+import { vbaseFor } from '@vtex/clients'
 
 import Getnet from './getnet'
+import { GETNET_AUTHORIZATION_BUCKET, GETNET_CANCELLATION_BUCKET, GETNET_CAPTURE_BUCKET, GETNET_REFUND_BUCKET, GETNET_REQUEST_BUCKET } from '../utils/constants'
 
 const TWO_RETRIES = 2
 const TIMEOUT_MS = 15000
@@ -16,10 +18,37 @@ const cacheStorage = new LRUCache<string, any>({ max: 5000 })
 // eslint-disable-next-line no-undef
 metrics.trackCache('wordpressProxy', cacheStorage)
 
+const GetnetAuth = vbaseFor<string, any>(GETNET_AUTHORIZATION_BUCKET)
+const GetnetRequest = vbaseFor<string, any>(GETNET_REQUEST_BUCKET)
+const GetnetCancellation = vbaseFor<string, any>(GETNET_CANCELLATION_BUCKET)
+const GetnetRefund = vbaseFor<string, any>(GETNET_REFUND_BUCKET)
+const GetnetCapture = vbaseFor<string, any>(GETNET_CAPTURE_BUCKET)
+
 export class Clients extends IOClients {
   public get getnet() {
     return this.getOrSet('getnet', Getnet)
   }
+
+  public get getnetAuth() {
+    return this.getOrSet('getnetAuth', GetnetAuth)
+  }
+
+  public get getnetRequest() {
+    return this.getOrSet('getnetRequest', GetnetRequest)
+  }
+
+  public get getnetCancellation() {
+    return this.getOrSet('getnetCancellation', GetnetCancellation)
+  }
+
+  public get getnetRefund() {
+    return this.getOrSet('getnetRefund', GetnetRefund)
+  }
+
+  public get getnetCapture() {
+    return this.getOrSet('getnetCapture', GetnetCapture)
+  }
+
 }
 
 export const clients: ClientsConfig<Clients> = {
--- node/utils/constants.ts
+++ node/utils/constants.ts
@@ -1,6 +1,6 @@
 // VBASE BUCKETS
-export const GETNET_AUTHORIZATION_BUCKET = 'getnetAuth'
-export const GETNET_REQUEST_BUCKET = 'getnetRequest'
-export const GETNET_CANCELLATION_BUCKET = 'getnetCancellation'
-export const GETNET_REFUND_BUCKET = 'getnetRefund'
-export const GETNET_CAPTURE_BUCKET = 'getnetCapture'
+export const GETNET_AUTHORIZATION_BUCKET = 'gnAuth'
+export const GETNET_REQUEST_BUCKET = 'gnReq'
+export const GETNET_CANCELLATION_BUCKET = 'gnCanc'
+export const GETNET_REFUND_BUCKET = 'gnRef'
+export const GETNET_CAPTURE_BUCKET = 'gnCap'
